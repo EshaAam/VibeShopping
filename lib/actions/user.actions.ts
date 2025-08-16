6061058eed25a65acb7b352bdb069c885d33b26a
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-08-17 03:51:57 +0600

    backend for signup

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,9 @@
-'use server';
+"use server";
 import { signIn, signOut } from "@/auth";
-import { signInFormSchema } from "../validator";
+import { signInFormSchema, signUpFormSchema } from "../validator";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -14,7 +16,7 @@ export async function signInWithCredentials(
       password: formData.get("password"),
     });
     await signIn("credentials", user);
-    
+
     return { success: true, message: "Signed in successfully!" };
   } catch (error) {
     if (isRedirectError(error)) {
@@ -23,6 +25,42 @@ export async function signInWithCredentials(
     return { success: false, message: "Invalid email or username" };
   }
 }
+
+//register a new user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+    const plainPassword = formData.get("password") as string;
+
+    user.password = hashSync(user.password, 10);
+    // Create the user in the database
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "Signed up successfully!" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: "Invalid email or username" };
+  }
+}
+
 // Sign the user out
 export async function signOutUser() {
   await signOut();
--- lib/validator.ts
+++ lib/validator.ts
@@ -1,12 +1,17 @@
-import { z } from 'zod';
+import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 // Schema for inserting a product
 // This schema defines the structure and validation rules for product data
 // It ensures that all required fields are present and meet specified criteria
 // The schema is used to validate product data before it is inserted into the database
-const currency = z.string().refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), {
-  message: "Price must be a valid currency format with two decimal places",
-});
+const currency = z
+  .string()
+  .refine(
+    (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+    {
+      message: "Price must be a valid currency format with two decimal places",
+    }
+  );
 //schema for inserting a product
 export const insertProductSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
@@ -23,6 +28,24 @@ export const insertProductSchema = z.object({
 
 //schema for signing in users
 export const signInFormSchema = z.object({
-  email: z.string().email("Invalid email address").min(3, "Email must be at least 3 characters"),
+  email: z
+    .string()
+    .email("Invalid email address")
+    .min(3, "Email must be at least 3 characters"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
+
+// Schema for signing up a user
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
