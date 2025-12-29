73ec3c222b65397f7e555e0543c04072249be180
Ekramul Esham <112435061+EshaAam@users.noreply.github.com>
2025-12-29 16:59:05 +0600

    product carousel fixed

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -1,26 +1,28 @@
-'use client';
+"use client";
 
-import Autoplay from 'embla-carousel-autoplay';
+import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from '@/components/ui/carousel';
-import { Product } from '@/types';
-import Link from 'next/link';
-import Image from 'next/image';
+} from "@/components/ui/carousel";
+import { Product } from "@/types";
+import Link from "next/link";
+import Image from "next/image";
 
 export function ProductCarousel({ data }: { data: Product[] }) {
   // Filter products that have banner images
-  const productsWithBanners = data.filter((product) => product.banner && product.banner.trim() !== '');
+  const productsWithBanners = data.filter(
+    (product) => product.banner && product.banner.trim() !== ""
+  );
 
   if (productsWithBanners.length === 0) return null;
 
   return (
     <Carousel
-      className='w-full mb-12'
+      className="w-full mb-12"
       opts={{
         loop: true,
       }}
@@ -36,19 +38,23 @@ export function ProductCarousel({ data }: { data: Product[] }) {
         {productsWithBanners.map((product: Product) => (
           <CarouselItem key={product.id}>
             <Link href={`/product/${product.slug}`}>
-              <div className='relative mx-auto'>
+              <div className="relative mx-auto h-56 sm:h-64 md:h-72 lg:h-80">
                 {product.banner && (
                   <Image
                     alt={product.name}
                     src={product.banner}
-                    width={1920}
-                    height={680}
-                    className='w-full h-auto'
+                    // width={1920}
+                    // height={200}
+                    // className='w-full h-auto'
+                    // priority
+                    fill
+                    sizes="100vw"
+                    className="object-cover"
                     priority
                   />
                 )}
-                <div className='absolute inset-0 flex items-end justify-center'>
-                  <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
+                <div className="absolute inset-0 flex items-end justify-center">
+                  <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white">
                     {product.name}
                   </h2>
                 </div>
